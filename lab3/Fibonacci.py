6f1f86125fbec516e4721ec01f8a231dad45569e
Ben <bendaan22@yahoo.com>
2015-09-30 15:23:41 +0000

    Fibonacci

--- lab3/Fibonacci.py
+++ lab3/Fibonacci.py
@@ -2,3 +2,13 @@
 # sum of two elements defines the next
 #EULER challenges
 
+num = 0
+nextNum = 1
+
+print("The sequence is \n")
+while nextNum < 10:
+  print(nextNum, end =',')
+  #num = nextNum
+  #nextNum = num + nextNum
+  num, nextNum = nextNum, num + nextNum
+
