52d4c12f5f423b199d3a66a6c7ae34911913b4b8
Ben <bendaan22@yahoo.com>
2015-09-30 15:43:01 +0000

    Fibonacci

--- lab3/Fibonacci.py
+++ lab3/Fibonacci.py
@@ -2,11 +2,16 @@
 # sum of two elements defines the next
 #EULER challenges
 
+#make the variables to be added
 num = 0
 nextNum = 1
 
 print("The sequence is \n")
+#use while loop to loop through the sequence
 while nextNum < 20:
   print(nextNum, end =',')
 
+  #this puts the first element as the second element
+  #such as num will be nextNum
+  #then nextNum will be the combination of the past two numbers
   num, nextNum = nextNum, num + nextNum
