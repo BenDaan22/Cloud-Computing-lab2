a435c45c85d6f7748d31770ac17afbf9f0036618
Ben <bendaan22@yahoo.com>
2015-09-30 15:00:45 +0000

    Palindrome

---
+++ lab3/palindrome.py
@@ -0,0 +1,14 @@
+#palindrome codes
+
+#input a word
+text = raw_input("Input a word")
+
+#to see the word in reverse
+word_rev = reversed(text)
+
+#to see if the word is a palindrome
+if list(text) == list(word_rev):
+  print("This is a palindrome")
+
+else :
+  print("This is not a palindrome")
